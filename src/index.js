783747d454574a6d1aefdf61a28925501732dde6
anna-blumenkrantz <91938764+anna-blumenkrantz@users.noreply.github.com>
2023-02-05 19:16:21 +0200

    init: donetowelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    // Check if matrix has been passed as a parameter
+  if (!matrix) {
+    return [];
+  }
+  let result = [];
+  
+  // Loop through each row of the matrix
+  for (let i = 0; i < matrix.length; i++) {
+    // Check if the current row is defined and has elements
+    if (!matrix[i] || !matrix[i].length) {
+      continue;
+    }
+    // Loop through each element in the current row
+    for (let j = 0; j < matrix[i].length; j++) {
+      // Calculate the column index based on the current row and iteration
+      let columnIdx = i % 2 === 0 
+        ? j
+        : (matrix[i].length - j - 1);
+
+      // Push the current element to the result array
+      result.push(matrix[i][columnIdx]);
+    }
+  }
+  return result;
 }
